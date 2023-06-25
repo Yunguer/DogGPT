765c506bb83eea8fece847214b0c2a28723d4a2f
Yunguer Dvorak <74862406+Yunguer@users.noreply.github.com>
2023-06-25 19:16:03 -0300

    Delete PW directory

--- PW/src/src/hooks/useAuth.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/auth";
-
-export default function(){
-    const context = useContext(AuthContext);
-
-    return context;
-}
